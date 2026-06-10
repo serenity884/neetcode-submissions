ef13f5ece65afd52120a2fa6cb8aa239ed055c36
serenity884 <rajendra_pawar@outlook.com>
2026-06-10 13:07:54 -0500

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                while (j < n) {
+                    dp[j++] = 0;
+                }
+                continue;
+            }
+            dp[j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else if(j ==0){
+                    continue;
+                }else {
+                    dp[j] = dp[j - 1] + dp[j];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
