abcddfcc77fb56f904108ffc4e120017d86d85ff
serenity884 <rajendra_pawar@outlook.com>
2026-06-22 19:51:19 -0500

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+        int prev2 = 1; // ways(0)
+        int prev1 = 1; // ways(1)
+        int curr = 0;
+        for (int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+}
